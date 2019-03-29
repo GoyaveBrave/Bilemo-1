2ff9aba403effec4d0a1a7f815329bfdbd111358
percevalseb1309 <percevalseb@gmail.com>
2019-03-29 13:32:19 +0100

    validate errors #12

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class UserController extends AbstractFOSRestController
 {
@@ -72,11 +73,21 @@ class UserController extends AbstractFOSRestController
      *     path = "/users",
      *     name = "user_create",
      * )
-     * @ParamConverter("user", converter="fos_rest.request_body")
+     * @ParamConverter(
+     *     "user",
+     *      converter="fos_rest.request_body",
+     *      options={
+     *         "validator"={"groups"="create"}
+     *     }
+     * )
      * @Rest\View(statusCode=Response::HTTP_CREATED, serializerGroups={"details"})
      */
-    public function create(User $user)
+    public function create(User $user, ConstraintViolationListInterface $violations)
     {
+        if (count($violations) > 0) {
+            return $this->view($violations, Response::HTTP_BAD_REQUEST);
+        }
+
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($user);
         $entityManager->flush();
