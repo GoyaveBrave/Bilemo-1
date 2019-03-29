e6f8c60f30464fc03774d96fc8d66a6598131c09
percevalseb1309 <percevalseb@gmail.com>
2019-03-29 12:28:43 +0100

    add pagination for the list of users #12

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -2,21 +2,14 @@
 
 namespace App\Repository;
 
-use App\Entity\Phone;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 
-class AbstractRepository extends ServiceEntityRepository
+class AbstractRepository extends EntityRepository
 {
-    public function __construct(RegistryInterface $registry)
-    {
-        parent::__construct($registry, Phone::class);
-    }
-    
-    protected function paginate(QueryBuilder $qb, $limit = 5, $offset = 0)
+    protected function paginate(QueryBuilder $qb, $limit, $offset = 0)
     {
         if (0 === $limit || 0 === $offset) {
             throw new \LogicException('$limit & $offset must be greater than 0.');
--- src/Repository/PhoneRepository.php
+++ src/Repository/PhoneRepository.php
@@ -2,12 +2,6 @@
 
 namespace App\Repository;
 
-/**
- * @method Phone|null find($id, $lockMode = null, $lockVersion = null)
- * @method Phone|null findOneBy(array $criteria, array $orderBy = null)
- * @method Phone[]    findAll()
- * @method Phone[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class PhoneRepository extends AbstractRepository
 {
     public function search($term, $order = 'asc', $limit = 5, $offset = 0)
@@ -28,33 +22,4 @@ class PhoneRepository extends AbstractRepository
         
         return $this->paginate($qb, $limit, $offset);
     }
-
-    // /**
-    //  * @return Phone[] Returns an array of Phone objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Phone
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
