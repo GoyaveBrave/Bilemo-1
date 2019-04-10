e93f8f77d44821b39522aa55a1da2315ad9675c8
percevalseb1309 <percevalseb@gmail.com>
2019-04-10 20:39:37 +0200

    document the api #37

--- src/Representation/Phones.php
+++ src/Representation/Phones.php
@@ -34,7 +34,7 @@ class Phones
         $this->addMeta('total_items', $data->getNbResults());
         if ($data->hasPreviousPage()) {
             $this->addMeta('previous_page', $data->getPreviousPage());
-        }        
+        }
         $this->addMeta('current_page', $data->getCurrentPage());
         if ($data->hasNextPage()) {
             $this->addMeta('next_page', $data->getNextPage());
